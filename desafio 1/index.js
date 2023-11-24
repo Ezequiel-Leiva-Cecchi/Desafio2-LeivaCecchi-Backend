2025cd885c925ee756cbaf5cdd05ab5d03097406
Ezequiel Leiva Cecchi <ezequielleivacecchi@gmail.com>
2023-11-24 18:17:49 -0300

    Desafio 2

---
+++ desafio 1/index.js
@@ -0,0 +1,83 @@
+class ProductManager {
+    constructor() {
+        // Inicializa la lista de productos.
+        this.products = [];
+    }
+
+    static id = 0;
+
+    // Método para agregar productos con validación de código único.
+    addProduct(title, description, price, stock, code, image) {
+        // Validación: Verifica si el código del producto ya existe en la lista.
+        const codeExists = this.products.find(product => product.code === code);
+        if (codeExists) {
+            throw new Error(`El código ${code} se está repitiendo`);
+        }
+
+        // Validación: Verifica que no haya campos undefined en el nuevo producto.
+        if (title && description && price && stock && code && image) {
+            // Incrementa el ID estático y agrega el nuevo producto a la lista.
+            ProductManager.id++;
+            this.products.push({
+                id: ProductManager.id,
+                title,
+                description,
+                price,
+                stock,
+                code,
+                image
+            });
+        } else {
+            throw new Error("Todos los campos son requeridos.");
+        }
+    }
+
+    // Método para obtener la lista completa de productos.
+    getProducts() {
+        return this.products;
+    }
+
+    // Método para buscar un producto por su ID.
+    getProductById(id) {
+        const product = this.products.find(product => product.id === id);
+        // Validación: Si el producto no se encuentra, lanza un error.
+        if (!product) {
+            throw new Error("No se encuentra el producto");
+        } else {
+            return product;
+        }
+    }
+}
+
+// Crear una instancia de la clase ProductManager.
+const productos = new ProductManager();
+
+// Prueba 1: Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío [].
+const productsEmpty = productos.getProducts();
+console.log("Prueba 1:", productsEmpty);
+
+// Prueba 2: Se llamará al método “addProduct” con ciertos campos y se verificará que se agregue satisfactoriamente.
+productos.addProduct("producto prueba", "Este es un producto prueba", 200, 25, "abc123", "Sin imagen");
+
+// Prueba 3: Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado.
+const productsAfterAdd = productos.getProducts();
+console.log("Prueba 3:", productsAfterAdd);
+
+// Prueba 4: Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
+try {
+    productos.addProduct("producto prueba", "Este es un producto prueba", 200, 25, "abc123", "Sin imagen");
+} catch (error) {
+    console.log("Prueba 4:", error.message);
+}
+
+// Prueba 5: Se evaluará que getProductById devuelva error si no encuentra el producto.
+try {
+    const nonExistentProduct = productos.getProductById(3);
+    console.log("Prueba 5:", nonExistentProduct);
+} catch (error) {
+    console.log("Prueba 5:", error.message);
+}
+
+// Prueba 6: Se evaluará que getProductById devuelva el producto si lo encuentra.
+const existingProduct = productos.getProductById(1); // Se puede ajustar el ID según el producto que se haya agregado.
+console.log("Prueba 6:", existingProduct);
---
+++ fileSystem.js
@@ -0,0 +1,133 @@
+// Importa el módulo fs de Node.js para trabajar con el sistema de archivos.
+import { promises as fs } from "fs";
+
+// Clase ProductManager para gestionar productos.
+class ProductManager {
+    // Constructor que inicializa la ubicación del archivo y la lista de productos.
+    constructor() {
+        this.patch = "./products.txt";  // Ruta del archivo de productos.
+        this.products = [];  // Lista de productos.
+    }
+
+    // Atributo estático para generar IDs únicos.
+    static id = 0;
+
+    // Método para agregar un producto.
+    addProduct = async (title, description, price, stock, code, image) => {
+        ProductManager.id++;
+
+        // Crea un nuevo objeto de producto.
+        let nuevosProducts = {
+            title,
+            description,
+            price,
+            stock,
+            code,
+            image,
+            id: ProductManager.id
+        };
+
+        // Agrega el nuevo producto a la lista.
+        this.products.push(nuevosProducts);
+
+        // Escribe la lista de productos en el archivo.
+        await fs.writeFile(this.patch, JSON.stringify(this.products));
+    };
+
+    // Método para leer los productos desde el archivo.
+    readProducts = async () => {
+        try {
+            let answer = await fs.readFile(this.patch, "utf-8");
+
+            // Verifica si el contenido del archivo está vacío.
+            if (!answer.trim()) {
+                return [];
+            }
+
+            // Parsea el contenido del archivo como JSON.
+            return JSON.parse(answer);
+        } catch (error) {
+            // Maneja errores al leer o analizar el archivo.
+            console.error("Error al leer o analizar el archivo:", error.message);
+            return [];
+        }
+    };
+
+    // Método para obtener la lista completa de productos.
+    getProducts = async () => {
+        let secondAnswer = await this.readProducts();
+        return secondAnswer;
+    };
+
+    // Método para obtener un producto por su ID.
+    getProductsById = async (id) => {
+        let thirdAnswer = await this.readProducts();
+        const foundProduct = thirdAnswer.find(productos => productos.id === id);
+
+        if (!foundProduct) {
+            console.log("No se logró encontrar ningún producto");
+        } else {
+            console.log(foundProduct);
+        }
+    };
+
+    // Método para eliminar un producto por su ID.
+    deleteProductsById = async (id) => {
+        let thirdAnswer = await this.readProducts();
+        const filter = thirdAnswer.filter(productos => productos.id != id);
+
+        // Escribe la lista filtrada en el archivo.
+        await fs.writeFile(this.patch, JSON.stringify(filter));
+        console.log("Producto eliminado");
+    };
+
+    // Método para actualizar un producto por su ID.
+    updateProducts = async (updatedFiles) => {
+        if (!updatedFiles.id) {
+            console.log("Se debe proporcionar el ID del producto a actualizar");
+            return;
+        }
+
+        let thirdAnswer = await this.readProducts();
+        const index = thirdAnswer.findIndex(productos => productos.id === updatedFiles.id);
+
+        if (index === -1) {
+            console.log("No se logró encontrar el producto para actualizar");
+            return;
+        }
+
+        // Actualiza los campos del producto encontrado.
+        thirdAnswer[index] = { ...thirdAnswer[index], ...updatedFiles };
+
+        // Escribe la lista actualizada en el archivo.
+        await fs.writeFile(this.patch, JSON.stringify(thirdAnswer));
+        console.log("Producto actualizado");
+    };
+}
+
+// Crear una instancia de la clase ProductManager.
+const productos = new ProductManager();
+
+// testing:
+
+// Prueba 1: Obtener la lista de productos (inicialmente vacía).
+productos.getProducts().then((result) => {
+    console.log("Prueba getProducts:", result);
+});
+
+// Prueba 2: Agregar un nuevo producto.
+productos.addProduct("producto prueba", "Este es un producto prueba", 200, 25, "abc123", "Sin imagen");
+
+// Prueba 3: Obtener la lista de productos después de agregar un producto.
+productos.getProducts().then((result) => {
+    console.log("Prueba getProducts después de agregar producto:", result);
+});
+
+// Prueba 4: Obtener un producto por su ID (en este caso, el ID es 1).
+productos.getProductsById(1);
+
+// Prueba 5: Actualizar un producto (en este caso, cambiar el precio del producto con ID 1 a 250).
+productos.updateProducts({ id: 1, price: 250 });
+
+// Prueba 6: Eliminar un producto por su ID (en este caso, eliminar el producto con ID 1).
+productos.deleteProductsById(1);
