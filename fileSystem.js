a25b79bf1499265dca400bdf643368203b9dec19
Ezequiel Leiva Cecchi <ezequielleivacecchi@gmail.com>
2023-11-30 12:38:27 -0300

    Desafio 2

--- fileSystem.js
+++ fileSystem.js
@@ -1,12 +1,11 @@
-// Importa el módulo fs de Node.js para trabajar con el sistema de archivos.
 import { promises as fs } from "fs";
 
 // Clase ProductManager para gestionar productos.
 class ProductManager {
     // Constructor que inicializa la ubicación del archivo y la lista de productos.
     constructor() {
-        this.patch = "./products.txt";  // Ruta del archivo de productos.
-        this.products = [];  // Lista de productos.
+        this.patch = "./products.txt";
+        this.products = [];
     }
 
     // Atributo estático para generar IDs únicos.
@@ -14,9 +13,22 @@ class ProductManager {
 
     // Método para agregar un producto.
     addProduct = async (title, description, price, stock, code, image) => {
+        // Validar que todos los campos estén presentes.
+        if (!title || !description || !price || !stock || !code || !image) {
+            console.log("Todos los campos son obligatorios");
+            return;
+        }
+
+        // Validar que el código del producto sea único.
+        if (this.products.some(product => product.code === code)) {
+            console.log("Ya existe un producto con este código");
+            return;
+        }
+
+        // Incrementar el ID estático para obtener uno único.
         ProductManager.id++;
 
-        // Crea un nuevo objeto de producto.
+        // Crear un nuevo objeto de producto.
         let nuevosProducts = {
             title,
             description,
@@ -27,27 +39,28 @@ class ProductManager {
             id: ProductManager.id
         };
 
-        // Agrega el nuevo producto a la lista.
+        // Agregar el nuevo producto a la lista.
         this.products.push(nuevosProducts);
 
-        // Escribe la lista de productos en el archivo.
+        // Escribir la lista de productos en el archivo.
         await fs.writeFile(this.patch, JSON.stringify(this.products));
     };
 
     // Método para leer los productos desde el archivo.
     readProducts = async () => {
         try {
+            // Leer el contenido del archivo.
             let answer = await fs.readFile(this.patch, "utf-8");
 
-            // Verifica si el contenido del archivo está vacío.
+            // Verificar si el contenido del archivo está vacío.
             if (!answer.trim()) {
                 return [];
             }
 
-            // Parsea el contenido del archivo como JSON.
+            // Parsear el contenido del archivo como JSON.
             return JSON.parse(answer);
         } catch (error) {
-            // Maneja errores al leer o analizar el archivo.
+            // Manejar errores al leer o analizar el archivo.
             console.error("Error al leer o analizar el archivo:", error.message);
             return [];
         }
@@ -55,13 +68,17 @@ class ProductManager {
 
     // Método para obtener la lista completa de productos.
     getProducts = async () => {
+        // Obtener la lista de productos desde el archivo.
         let secondAnswer = await this.readProducts();
         return secondAnswer;
     };
 
     // Método para obtener un producto por su ID.
     getProductsById = async (id) => {
+        // Obtener la lista de productos desde el archivo.
         let thirdAnswer = await this.readProducts();
+
+        // Encontrar el producto con el ID proporcionado.
         const foundProduct = thirdAnswer.find(productos => productos.id === id);
 
         if (!foundProduct) {
@@ -73,30 +90,37 @@ class ProductManager {
 
     // Método para eliminar un producto por su ID.
     deleteProductsById = async (id) => {
+        // Obtener la lista de productos desde el archivo.
         let thirdAnswer = await this.readProducts();
+
+        // Encontrar el producto con el ID proporcionado.
         const foundProduct = thirdAnswer.find(productos => productos.id === id);
 
         if (!foundProduct) {
             console.log("No se logró encontrar el producto para eliminar");
             return;
-        }else{
+        } else {
+            // Filtrar la lista para excluir el producto con el ID proporcionado.
             const filter = thirdAnswer.filter(productos => productos.id !== id);
 
-            // Escribe la lista filtrada en el archivo.
+            // Escribir la lista filtrada en el archivo.
             await fs.writeFile(this.patch, JSON.stringify(filter));
             console.log("Producto eliminado");
         }
     };
 
-
     // Método para actualizar un producto por su ID.
     updateProducts = async (updatedFiles) => {
+        // Validar que se proporcione el ID del producto a actualizar.
         if (!updatedFiles.id) {
             console.log("Se debe proporcionar el ID del producto a actualizar");
             return;
         }
 
+        // Obtener la lista de productos desde el archivo.
         let thirdAnswer = await this.readProducts();
+
+        // Encontrar el índice del producto con el ID proporcionado.
         const index = thirdAnswer.findIndex(productos => productos.id === updatedFiles.id);
 
         if (index === -1) {
@@ -104,10 +128,16 @@ class ProductManager {
             return;
         }
 
-        // Actualiza los campos del producto encontrado.
+        // Validar que el nuevo código (si se proporciona) sea único.
+        if (updatedFiles.code && thirdAnswer.some(product => product.code === updatedFiles.code)) {
+            console.log("Ya existe un producto con este código");
+            return;
+        }
+
+        // Actualizar los campos del producto encontrado.
         thirdAnswer[index] = { ...thirdAnswer[index], ...updatedFiles };
 
-        // Escribe la lista actualizada en el archivo.
+        // Escribir la lista actualizada en el archivo.
         await fs.writeFile(this.patch, JSON.stringify(thirdAnswer));
         console.log("Producto actualizado");
     };
@@ -116,7 +146,7 @@ class ProductManager {
 // Crear una instancia de la clase ProductManager.
 const productos = new ProductManager();
 
-// testing:
+// Pruebas:
 
 // Prueba 1: Obtener la lista de productos (inicialmente vacía).
 productos.getProducts().then((result) => {
